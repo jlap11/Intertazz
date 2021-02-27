4a6ae86070eec27032f3858e53bf5b97bf4d3159
Yesid <yesidamaya34@gmail.com>
2021-02-27 14:29:36 -0500

    Formulario Main V1

--- Intertazz/Formularios/Main.Designer.cs
+++ Intertazz/Formularios/Main.Designer.cs
@@ -36,13 +36,26 @@ namespace Intertazz.Formularios
             this.btnMinimizar = new System.Windows.Forms.PictureBox();
             this.btnCerrar = new System.Windows.Forms.PictureBox();
             this.PMenuVertical = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnVentas = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnProductos = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnInventario = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.PContenedor = new System.Windows.Forms.Panel();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.btnReportes = new System.Windows.Forms.Button();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.btnPagos = new System.Windows.Forms.Button();
             this.PBarraTitulo.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.btnRestaurar)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.btnMaximizar)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.btnMinimizar)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.btnCerrar)).BeginInit();
+            this.PMenuVertical.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // PBarraTitulo
@@ -57,11 +70,12 @@ namespace Intertazz.Formularios
             this.PBarraTitulo.Name = "PBarraTitulo";
             this.PBarraTitulo.Size = new System.Drawing.Size(1300, 35);
             this.PBarraTitulo.TabIndex = 0;
+            this.PBarraTitulo.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PBarraTitulo_MouseDown);
             // 
             // btnRestaurar
             // 
             this.btnRestaurar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnRestaurar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnRestaurar.Cursor = System.Windows.Forms.Cursors.WaitCursor;
             this.btnRestaurar.Image = ((System.Drawing.Image)(resources.GetObject("btnRestaurar.Image")));
             this.btnRestaurar.Location = new System.Drawing.Point(1241, 0);
             this.btnRestaurar.Name = "btnRestaurar";
@@ -69,6 +83,7 @@ namespace Intertazz.Formularios
             this.btnRestaurar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.btnRestaurar.TabIndex = 3;
             this.btnRestaurar.TabStop = false;
+            this.btnRestaurar.UseWaitCursor = true;
             this.btnRestaurar.Visible = false;
             this.btnRestaurar.Click += new System.EventHandler(this.btnRestaurar_Click);
             // 
@@ -114,11 +129,110 @@ namespace Intertazz.Formularios
             // PMenuVertical
             // 
             this.PMenuVertical.BackColor = System.Drawing.Color.Black;
+            this.PMenuVertical.Controls.Add(this.panel5);
+            this.PMenuVertical.Controls.Add(this.btnPagos);
+            this.PMenuVertical.Controls.Add(this.panel4);
+            this.PMenuVertical.Controls.Add(this.btnReportes);
+            this.PMenuVertical.Controls.Add(this.panel3);
+            this.PMenuVertical.Controls.Add(this.btnVentas);
+            this.PMenuVertical.Controls.Add(this.panel2);
+            this.PMenuVertical.Controls.Add(this.btnProductos);
+            this.PMenuVertical.Controls.Add(this.panel1);
+            this.PMenuVertical.Controls.Add(this.btnInventario);
+            this.PMenuVertical.Controls.Add(this.pictureBox1);
             this.PMenuVertical.Dock = System.Windows.Forms.DockStyle.Left;
             this.PMenuVertical.Location = new System.Drawing.Point(0, 35);
             this.PMenuVertical.Name = "PMenuVertical";
             this.PMenuVertical.Size = new System.Drawing.Size(220, 615);
             this.PMenuVertical.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.DodgerBlue;
+            this.panel3.Location = new System.Drawing.Point(11, 204);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(10, 31);
+            this.panel3.TabIndex = 6;
+            // 
+            // btnVentas
+            // 
+            this.btnVentas.BackColor = System.Drawing.Color.Black;
+            this.btnVentas.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnVentas.FlatAppearance.BorderSize = 0;
+            this.btnVentas.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DodgerBlue;
+            this.btnVentas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVentas.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVentas.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnVentas.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnVentas.Location = new System.Drawing.Point(20, 204);
+            this.btnVentas.Name = "btnVentas";
+            this.btnVentas.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.btnVentas.Size = new System.Drawing.Size(188, 31);
+            this.btnVentas.TabIndex = 5;
+            this.btnVentas.Text = "Ventas";
+            this.btnVentas.UseVisualStyleBackColor = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.DodgerBlue;
+            this.panel2.Location = new System.Drawing.Point(11, 167);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(10, 31);
+            this.panel2.TabIndex = 4;
+            // 
+            // btnProductos
+            // 
+            this.btnProductos.BackColor = System.Drawing.Color.Black;
+            this.btnProductos.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnProductos.FlatAppearance.BorderSize = 0;
+            this.btnProductos.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DodgerBlue;
+            this.btnProductos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnProductos.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProductos.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnProductos.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnProductos.Location = new System.Drawing.Point(20, 167);
+            this.btnProductos.Name = "btnProductos";
+            this.btnProductos.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.btnProductos.Size = new System.Drawing.Size(188, 31);
+            this.btnProductos.TabIndex = 3;
+            this.btnProductos.Text = "Productos";
+            this.btnProductos.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DodgerBlue;
+            this.panel1.Location = new System.Drawing.Point(11, 130);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(10, 31);
+            this.panel1.TabIndex = 2;
+            // 
+            // btnInventario
+            // 
+            this.btnInventario.BackColor = System.Drawing.Color.Black;
+            this.btnInventario.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnInventario.FlatAppearance.BorderSize = 0;
+            this.btnInventario.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DodgerBlue;
+            this.btnInventario.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnInventario.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInventario.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnInventario.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnInventario.Location = new System.Drawing.Point(20, 130);
+            this.btnInventario.Name = "btnInventario";
+            this.btnInventario.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.btnInventario.Size = new System.Drawing.Size(188, 31);
+            this.btnInventario.TabIndex = 1;
+            this.btnInventario.Text = "Inventario";
+            this.btnInventario.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(202, 109);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
             // PContenedor
             // 
@@ -127,6 +241,58 @@ namespace Intertazz.Formularios
             this.PContenedor.Name = "PContenedor";
             this.PContenedor.Size = new System.Drawing.Size(1080, 615);
             this.PContenedor.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.DodgerBlue;
+            this.panel4.Location = new System.Drawing.Point(11, 241);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(10, 31);
+            this.panel4.TabIndex = 8;
+            // 
+            // btnReportes
+            // 
+            this.btnReportes.BackColor = System.Drawing.Color.Black;
+            this.btnReportes.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnReportes.FlatAppearance.BorderSize = 0;
+            this.btnReportes.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DodgerBlue;
+            this.btnReportes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReportes.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReportes.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnReportes.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnReportes.Location = new System.Drawing.Point(20, 278);
+            this.btnReportes.Name = "btnReportes";
+            this.btnReportes.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.btnReportes.Size = new System.Drawing.Size(188, 31);
+            this.btnReportes.TabIndex = 7;
+            this.btnReportes.Text = "Reportes";
+            this.btnReportes.UseVisualStyleBackColor = false;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.DodgerBlue;
+            this.panel5.Location = new System.Drawing.Point(11, 278);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(10, 31);
+            this.panel5.TabIndex = 10;
+            // 
+            // btnPagos
+            // 
+            this.btnPagos.BackColor = System.Drawing.Color.Black;
+            this.btnPagos.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPagos.FlatAppearance.BorderSize = 0;
+            this.btnPagos.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DodgerBlue;
+            this.btnPagos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPagos.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPagos.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnPagos.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnPagos.Location = new System.Drawing.Point(21, 241);
+            this.btnPagos.Name = "btnPagos";
+            this.btnPagos.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.btnPagos.Size = new System.Drawing.Size(173, 31);
+            this.btnPagos.TabIndex = 9;
+            this.btnPagos.Text = "Pagos";
+            this.btnPagos.UseVisualStyleBackColor = false;
             // 
             // Main
             // 
@@ -144,6 +310,8 @@ namespace Intertazz.Formularios
             ((System.ComponentModel.ISupportInitialize)(this.btnMaximizar)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.btnMinimizar)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.btnCerrar)).EndInit();
+            this.PMenuVertical.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -158,5 +326,16 @@ namespace Intertazz.Formularios
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
         private System.Windows.Forms.PictureBox btnRestaurar;
         private System.Windows.Forms.PictureBox btnMaximizar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnInventario;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btnVentas;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnProductos;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button btnReportes;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Button btnPagos;
     }
 }
